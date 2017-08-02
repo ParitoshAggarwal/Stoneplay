c2e155dc01f2acd338ace4ced13d82a3e3098e00
ParitoshAggarwal <paritoshmait@gmail.com>
2017-08-03 02:05:18 +0530

    generic classes

--- Django/firstWebsite/music/urls.py
+++ Django/firstWebsite/music/urls.py
@@ -6,11 +6,15 @@ app_name = "music"
 
 urlpatterns =[
     #music/
-    url(r'^$',views.index,name="index"),
+    url(r'^$',views.IndexView.as_view(),name="index"),
 
     #mucis/423/
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name="detail"),
+    # url(r'^(?P<album_id>[0-9]+)/$',views.detail,name="detail"),
 
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name="detail"),
+    
     #music/2343/favourite
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite")
+    # url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite")
+
+
 ]
--- Django/firstWebsite/music/views.py
+++ Django/firstWebsite/music/views.py
@@ -1,34 +1,48 @@
-# from django.http import HttpResponse,Http404
-from . models import Album,Songs
-# from django.template import loader
-from django.shortcuts import render,get_object_or_404
+# # from django.http import HttpResponse,Http404
+# from . models import Album,Songs
+# # from django.template import loader
+# from django.shortcuts import render,get_object_or_404
+#
+# def index(request):
+#     all_albums= Album.objects.all()
+#     # template = loader.get_template("music/index.html")
+#     context = {
+#         'all_album' : all_albums,
+#     }
+#     return render(request,"music/index.html",context)
+#     # return HttpResponse(template.render(context,request))
+#
+#
+# def detail(request,album_id):
+#     # try:
+#     #     album = Album.objects.get(pk=album_id)
+#     # except Album.DoesNotExist:
+#     #     raise Http404("MESSED UP")
+#     album = get_object_or_404(Album,pk=album_id)
+#     return render(request,"music/detail.html",{'album':album})
+#
+#
+# # def favourite(request,album_id):
+# #     album = get_object_or_404(Album,pk=album_id)
+# #     try:
+# #         selected_song = album.songs_set.get(pk=request.POST['song'])
+# #     except Songs.DoesNotExist:
+# #         return render(request,"music/detail.html",{"album":album,"error_message":"Messed Up Something"})
+# #     else:
+# #         selected_song.is_favourite=True
+# #         selected_song.save()
+# #         return render(request, "music/detail.html", {'album': album})
+from django.views import generic
+from . models import Album
 
-def index(request):
-    all_albums= Album.objects.all()
-    # template = loader.get_template("music/index.html")
-    context = {
-        'all_album' : all_albums,
-    }
-    return render(request,"music/index.html",context)
-    # return HttpResponse(template.render(context,request))
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_album'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def detail(request,album_id):
-    # try:
-    #     album = Album.objects.get(pk=album_id)
-    # except Album.DoesNotExist:
-    #     raise Http404("MESSED UP")
-    album = get_object_or_404(Album,pk=album_id)
-    return render(request,"music/detail.html",{'album':album})
-
-
-def favourite(request,album_id):
-    album = get_object_or_404(Album,pk=album_id)
-    try:
-        selected_song = album.songs_set.get(pk=request.POST['song'])
-    except Songs.DoesNotExist:
-        return render(request,"music/detail.html",{"album":album,"error_message":"Messed Up Something"})
-    else:
-        selected_song.is_favourite=True
-        selected_song.save()
-        return render(request, "music/detail.html", {'album': album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
